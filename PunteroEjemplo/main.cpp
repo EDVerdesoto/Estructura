6294dfc85160d9d3accc64b03461b1edecd8d42d
Edison Damian Verdesoto Segovia <132785383+EDVerdesoto@users.noreply.github.com>
2023-11-24 11:57:41 -0500

    Update main.cpp

--- PunteroEjemplo/main.cpp
+++ PunteroEjemplo/main.cpp
@@ -17,8 +17,10 @@ int main(int argc, char** argv) {
 	printf("\nEl valor de a en octal = %o", a);
 	
 	printf("\nDireccion de t = %p", &t);
-	t=t+1;
-	printf("\nDireccion de t = %p", &t);
-	
+	printf("\nDireccion de t = %p", &t+1);
+	*p = 100;
+	printf("\nEl valor de a = %d", a);
+	**q = 1000;
+	printf("\nEl valor de a = %d", a);
 	return 0;
 }
